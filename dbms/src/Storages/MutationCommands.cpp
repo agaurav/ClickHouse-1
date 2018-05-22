thomascool/ClickHouse

write mutations to ZK

dbms/src/Storages/MutationCommands.cpp
@@ -1,0 +1,85 @@
+#include <Storages/MutationCommands.h>
+#include <IO/Operators.h>
+#include <Parsers/ExpressionListParsers.h>
+#include <Parsers/formatAST.h>
+#include <Parsers/parseQuery.h>
+
+
+namespace DB
+{
+
+static String typeToString(MutationCommand::Type type)
+{
+    switch (type)
+    {
+    case MutationCommand::DELETE:     return "DELETE";
+    default:
+        throw Exception("Unknown mutation type: " + toString<int>(type), ErrorCodes::LOGICAL_ERROR);
+    }
+}
+
+void MutationCommand::writeText(WriteBuffer & out) const
+{
+    out << typeToString(type) << "\n";
+
+    switch (type)
+    {
+    case  MutationCommand::DELETE:
+        {
+            std::stringstream ss;
+            formatAST(*predicate, ss, /* hilite = */ false, /* one_line = */ true);
+            out << "predicate: " << ss.str() << "\n";
+            break;
+        }
+    default:
+        break;
+    }
+}
+
+void MutationCommand::readText(ReadBuffer & in)
+{
+    String type_str;
+    in >> type_str >> "\n";
+
+    if (type_str == "DELETE")
+    {
+        type = DELETE;
+
+        String predicate_str;
+        in >> "predicate: " >> predicate_str >> "\n";
+        ParserExpressionWithOptionalAlias p_expr(false);
+        predicate = parseQuery(
+            p_expr, predicate_str.data(), predicate_str.data() + predicate_str.length(), "mutation predicate");
+    }
+    else
+    {
+        /// We wouldn't know how to execute the mutation but at least we will be able to load it
+        /// and know that it is there (important for selection of merges).
+        type = UNKNOWN;
+    }
+}
+
+
+void MutationCommands::writeText(WriteBuffer & out) const
+{
+    out << "mutation commands count: " << commands.size() << "\n";
+    for (const MutationCommand & command : commands)
+    {
+        command.writeText(out);
+    }
+}
+
+void MutationCommands::readText(ReadBuffer & in)
+{
+    size_t count;
+    in >> "mutation commands count: " >> count >> "\n";
+
+    for (size_t i = 0; i < count; ++i)
+    {
+        MutationCommand command;
+        command.readText(in);
+        commands.push_back(std::move(command));
+    }
+}
+
+}

agaurav/ClickHouse-1

Conflicts:
	dbms/src/Common/ZooKeeper/ZooKeeper.h

dbms/src/Storages/MutationCommands.cpp
@@ -2,10 +2,67 @@
 #include <Storages/IStorage.h>
 #include <Interpreters/ExpressionAnalyzer.h>
 #include <Columns/FilterDescription.h>
+#include <IO/Operators.h>
+#include <Parsers/ExpressionListParsers.h>
+#include <Parsers/formatAST.h>
+#include <Parsers/parseQuery.h>
 
 
 namespace DB
 {
+
+namespace ErrorCodes
+{
+    extern const int UNKNOWN_MUTATION_COMMAND;
+}
+
+static String typeToString(MutationCommand::Type type)
+{
+    switch (type)
+    {
+        case MutationCommand::DELETE:     return "DELETE";
+        default:
+            throw Exception("Bad mutation type: " + toString<int>(type), ErrorCodes::LOGICAL_ERROR);
+    }
+}
+
+void MutationCommand::writeText(WriteBuffer & out) const
+{
+    out << typeToString(type) << "\n";
+
+    switch (type)
+    {
+    case  MutationCommand::DELETE:
+        {
+            std::stringstream ss;
+            formatAST(*predicate, ss, /* hilite = */ false, /* one_line = */ true);
+            out << "predicate: " << ss.str() << "\n";
+            break;
+        }
+        default:
+            throw Exception("Bad mutation type: " + toString<int>(type), ErrorCodes::LOGICAL_ERROR);
+    }
+}
+
+void MutationCommand::readText(ReadBuffer & in)
+{
+    String type_str;
+    in >> type_str >> "\n";
+
+    if (type_str == "DELETE")
+    {
+        type = DELETE;
+
+        String predicate_str;
+        in >> "predicate: " >> predicate_str >> "\n";
+        ParserExpressionWithOptionalAlias p_expr(false);
+        predicate = parseQuery(
+            p_expr, predicate_str.data(), predicate_str.data() + predicate_str.length(), "mutation predicate", 0);
+    }
+    else
+        throw Exception("Unknown mutation command: `" + type_str + "'", ErrorCodes::UNKNOWN_MUTATION_COMMAND);
+}
+
 
 void MutationCommands::validate(const IStorage & table, const Context & context)
 {
@@ -26,6 +83,31 @@ void MutationCommands::validate(const IStorage & table, const Context & context)
             default:
                 throw Exception("Bad mutation type: " + toString<int>(command.type), ErrorCodes::LOGICAL_ERROR);
         }
+    }
+}
+
+void MutationCommands::writeText(WriteBuffer & out) const
+{
+    out << "format version: 1\n"
+        << "count: " << commands.size() << "\n";
+    for (const MutationCommand & command : commands)
+    {
+        command.writeText(out);
+    }
+}
+
+void MutationCommands::readText(ReadBuffer & in)
+{
+    in >> "format version: 1\n";
+
+    size_t count;
+    in >> "count: " >> count >> "\n";
+
+    for (size_t i = 0; i < count; ++i)
+    {
+        MutationCommand command;
+        command.readText(in);
+        commands.push_back(std::move(command));
     }
 }
 